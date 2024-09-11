d973e3b776917b9ed1f40fedffbe049c3ae1eb31
anantha <anantha.rao@antgroup.com>
2024-09-11 08:50:01 +0800

    Added chorus function

---
+++ Section_4/Chorus.java
@@ -0,0 +1,16 @@
+public class Chorus {
+    public static void main(String[] args) {
+        // Chorus needs to be sung 5 times
+        for (int x = 0; x< 5;x++){
+            chorus();
+        }
+
+    }
+
+    private static void chorus(){
+        System.out.println("Don't blame it on the sunshine");
+        System.out.println("Don't blame it on the moonlight");
+        System.out.println("Don't blame it on good times");
+        System.out.println("Don't blame it on the boogie\n");
+    }
+}

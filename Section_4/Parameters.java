f804b22969738152413f7d941406bfda16a5cc46
anantha <anantha.rao@antgroup.com>
2024-09-12 08:46:21 +0800

    feat: Complete workbook 4.1

---
+++ Section_4/Parameters.java
@@ -0,0 +1,14 @@
+public class Parameters {
+
+    public static void main(String[] args) {
+        calculateArea(2.5, 4);
+
+    }
+
+    public static void calculateArea(double height, double width) {
+        double area = height * width;
+
+        System.out.println("Area is :" + area);
+    }
+
+}
